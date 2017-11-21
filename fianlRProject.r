70ba8dbc0e2e116e45a02999f96cec4baa4f116d
shahisagar20 <shahisagar20@gmail.com>
2017-11-20 23:57:34 -0800

    Changes in remove outliers
    Now it can take any values both intege and numeric

--- fianlRProject.r
+++ fianlRProject.r
@@ -10,6 +10,9 @@
 removeOutliers = function(x){
  j =1
  y = 0
+ if(class(x)=='integer'){
+   x = as.numeric(x)
+ }
   if(class(x) == 'numeric' & length(x) != 0){
     for(i in x){
       if(!is.na(i)){
@@ -36,13 +39,51 @@ removeOutliers = function(x){
     }
 }
 
-x = seq(0,100, by =1)
-x[1] = 500
-x[5] =500
-x[7] = NA
-length(removeOutliers(removeOutliers(x)))
 
+#This function will remove the NAs at the positions of the outliers and then putput the mean
+#of the data without outliers.
 ourmean = function(x){
-  y = removeOutliers(x)
+  j=1
+  y = 0
+  out = removeOutliers(x)
+  for(i in out){
+    if(!is.na(i)){
+      y[j] = i
+      j =j+1
+    }
+  }
+  return(mean(y))
   
 }
+
+
+ourmean(x)
+
+#Function to produce the kernel density plots
+kfunction = function(x){
+  out = removeOutliers(x)
+  m = ourmean(y)
+  
+  y = 0
+  j=1
+  for(i in out){
+    if(!is.na(i)){
+      y[j] = i
+      j =j+1
+    }
+  }
+  par(mfrow =c(2,3))
+  plot(density(y, kernel = "gaussian", bw = m), col = "red", main = "Gaussian K")
+  plot(density(y, kernel = "cosine", bw =m), col = "red", main = "Cosine K")
+  plot(density(y, kernel = "rectangular", bw =m), col = "red", main = "Rect K")
+  plot(density(y, kernel = "triangular", bw =m), col = "red", main = "Triangular K")
+  plot(density(y, kernel = "epanechnikov", bw =m), col = "red", main = "epanechnikov K")
+  plot(density(y, kernel = "biweight", bw =m), col = "red", main = "biweight K")
+  
+  
+}
+class(Cars93$Max.Price)
+kfunction(Cars93$MPG.city)
+class(x)
+class(as.numeric(Cars93$MPG.city))
+kfunction(Cars93$Max.Price)
